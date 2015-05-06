e5484106615c14e24533c0625c1cee198892b80e
Yosuke ONOUE <onoue@likr-lab.com>
2015-05-06 11:42:06 +0900

    Update vertex renderer

--- src/directives/network.js
+++ src/directives/network.js
@@ -5,6 +5,8 @@ import d3 from 'd3';
 import Graph from 'eg-graph/lib/graph';
 import Renderer from 'eg-graph/lib/renderer';
 import CircleVertexRenderer from 'eg-graph/lib/renderer/vertex-renderer/circle-vertex-renderer';
+import vertexFunction from 'eg-graph/lib/renderer/vertex-function';
+import layerMatrix from 'eg-graph/lib/layouter/sugiyama/misc/layer-matrix';
 
 class ConstantLayerAssignment {
   constructor(g) {
@@ -41,6 +43,98 @@ class ConstantLayerAssignment {
   }
 }
 
+class ExCircleVertexRenderer extends CircleVertexRenderer {
+  render() {
+    return (selection) => {
+      const vertexColor = this.vertexColor(),
+            r = this.r();
+      selection.each(function (data) {
+        const element = d3.select(this);
+        if (element.select('circle').empty()) {
+          element.append('circle')
+            .attr({
+              cx: d => d.px,
+              cy: d => d.py,
+              r: r,
+              opacity: 0.5,
+              fill: vertexFunction(vertexColor)
+            });
+
+          element.append('text')
+            .text((d) => d.data.name)
+            .attr({
+              dx: r,
+              x: (d) => d.px,
+              y: (d) => d.py,
+              fill: vertexFunction(vertexColor)
+            });
+
+          element.on('click', (a, b, c) => {
+            data.data.selected = !data.data.selected;
+            d3.selectAll('g.vertex circle')
+              .attr('opacity', (d) => d.data.selected ? 1 : 0.5);
+            d3.selectAll('g.edge path')
+              .attr('opacity', (d) => d.source.data.selected || d.target.data.selected ? 1 : 0.4);
+          });
+        }
+      });
+
+      selection.select('circle')
+        .attr({
+          cx: d => d.x,
+          cy: d => d.y,
+          r: r,
+          opacity: (d) => d.data.selected ? 1 : 0.5,
+          fill: vertexFunction(vertexColor)
+        });
+      selection.select('text')
+        .attr({
+          dx: r,
+          x: (d) => d.x,
+          y: (d) => d.y,
+          fill: vertexFunction(vertexColor)
+        });
+    };
+  }
+}
+
+
+const baryCenter = (g, h1, h2, inverse=false) => {
+  const centers = {},
+        n = h1.length,
+        m = h2.length,
+        a = layerMatrix(g, h1, h2),
+        cmp = (u, v) => {
+          let d;
+          return (d = centers[u] - centers[v]) === 0 ? u - v : d;
+        };
+  if (inverse) {
+    for (let i = 0; i < n; ++i) {
+      let sum = 0,
+          count = 0;
+      for (let j = 0; j < m; ++j) {
+        const aij = a[i * m + j];
+        count += aij;
+        sum += j * aij;
+      }
+      centers[h1[i]] = count ? sum / count : Infinity;
+    }
+    h1.sort(cmp);
+  } else {
+    for (let j = 0; j < m; ++j) {
+      let sum = 0,
+          count = 0;
+      for (let i = 0; i < n; ++i) {
+        const aij = a[i * m + j];
+        count += aij;
+        sum += i * aij;
+      }
+      centers[h2[j]] = count ? sum / count : Infinity;
+    }
+    h2.sort(cmp);
+  }
+};
+
 angular.module('riken')
   .directive('network', () => {
     return {
@@ -50,7 +144,7 @@ angular.module('riken')
 
         const r = 10;
         const renderer = new Renderer()
-          .vertexRenderer(new CircleVertexRenderer())
+          .vertexRenderer(new ExCircleVertexRenderer())
           .vertexVisibility(({u, d}) => {
             if (!params.groups[d.nameGroup] || !params.layers[d.group]) {
               return false;
@@ -82,16 +176,21 @@ angular.module('riken')
             if (ud.nameGroup === vd.nameGroup) {
               return ud.nameGroupColor;
             } else {
-              return 'black';
+              return '#ccc';
             }
           })
-          .edgeOpacity(({d}) => Math.abs(d.r));
+          .edgeOpacity(({ud, vd}) => ud.selected || vd.selected ? 1 : 0.4);
 
         renderer.layouter()
           .layerAssignment(new ConstantLayerAssignment(g))
-          .layerMargin(1000)
+          .layerMargin(200)
+          .vertexMargin(5)
+          .edgeMargin(5)
           .vertexWidth(() => r * 2)
           .vertexHeight(() => r * 2);
+        renderer.layouter()
+          .crossingReduction()
+          .method(baryCenter);
 
         const zoom = d3.behavior.zoom()
           .scaleExtent([0.05, 1])
