4cffe119a74893e70d970b16d0f1dba34c691eaf
Yosuke ONOUE <onoue@likr-lab.com>
2015-07-14 14:25:40 +0900

    Change to load data from file

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -6,26 +6,41 @@ import sem from 'semjs';
 import Graph from 'eg-graph/lib/graph';
 
 class MainController {
-  constructor(graph) {
-    this.graph = graph;
+  constructor($scope, $modal) {
+    this.$modal = $modal;
+    this.graph = new Graph();
     this.params = {
-      rMin: 0.6,
+      rMin: 0.65,
       layers: {},
       groups: {},
       showAll: false,
       edgeConcentration: true
     };
 
-    for (const u of graph.vertices()) {
-      const column = graph.vertex(u);
-      if (this.params.layers[column.group] === void 0) {
-        this.params.layers[column.group] = true;
+    this.handleLoadFile = (data) => {
+      const color = d3.scale.category20();
+      for (const {u, d} of data.vertices) {
+        d.nameGroupColor = color(d.nameGroup);
+        this.graph.addVertex(u, d);
       }
-      if (this.params.groups[column.nameGroup] === void 0) {
-        this.params.groups[column.nameGroup] = true;
+      for (const {u, v, d} of data.edges) {
+        this.graph.addEdge(u, v, d);
       }
-    }
+
+      for (const u of this.graph.vertices()) {
+        const column = this.graph.vertex(u);
+        if (this.params.layers[column.group] === void 0) {
+          this.params.layers[column.group] = true;
+        }
+        if (this.params.groups[column.nameGroup] === void 0) {
+          this.params.groups[column.nameGroup] = true;
+        }
+      }
+
+      $scope.$broadcast('update-graph');
+    };
   }
+
 }
 
 angular.module('riken')
@@ -33,23 +48,6 @@ angular.module('riken')
     $stateProvider.state('main', {
       controller: 'MainController',
       controllerAs: 'main',
-      resolve: {
-        graph: ($http) => {
-          const color = d3.scale.category20();
-          return $http.get('data/data.json')
-            .then(({data}) => {
-              const graph = new Graph();
-              for (const {u, d} of data.vertices) {
-                d.nameGroupColor = color(d.nameGroup);
-                graph.addVertex(u, d);
-              }
-              for (const {u, v, d} of data.edges) {
-                graph.addEdge(u, v, d);
-              }
-              return graph;
-            });
-        }
-      },
       templateUrl: 'partials/main.html',
       url: '/'
     });
--- src/directives/control.js
+++ src/directives/control.js
@@ -3,18 +3,82 @@
 import angular from 'angular';
 import d3 from 'd3';
 
+const dialogTemplate = `
+<div class="modal-header">
+  <h3>Select File</h3>
+</div>
+<div class="modal-body">
+  <form>
+    <div class="form-group">
+      <input type="file" file-model="jsonFile">
+    </div>
+  </form>
+</div>
+<div class="modal-footer">
+  <button class="btn btn-default" ng-click="cancel()">Cancel</button>
+  <button class="btn btn-danger" ng-click="ok(jsonFile)">OK</button>
+</div>
+`;
+
+angular.module('riken')
+  .directive('fileModel', ['$parse', ($parse) => {
+    return {
+      restrict: 'A',
+      link: (scope, element, attrs) => {
+        var model = $parse(attrs.fileModel);
+        var modelSetter = model.assign;
+        element.bind('change', () => {
+          scope.$apply(() => {
+            modelSetter(scope, element[0].files[0]);
+          });
+        });
+      }
+    };
+  }]);
+
 angular.module('riken')
   .directive('control', () => {
     return {
+      controller: ($scope, $modal) => {
+        $scope.loadFile = () => {
+          $modal
+            .open({
+              controller: ($scope, $modalInstance) => {
+                $scope.ok = (file) => {
+                  if (file) {
+                    $modalInstance.close(file);
+                  } else {
+                    $modalInstance.dismiss();
+                  }
+                };
+
+                $scope.cancel = () => {
+                  $modalInstance.dismiss();
+                };
+              },
+              template: dialogTemplate
+            })
+            .result
+            .then((file) => {
+              const reader = new FileReader();
+              reader.onload = (e) => {
+                $scope.onLoadFile(JSON.parse(e.target.result));
+              };
+              reader.readAsText(file);
+            });
+        };
+      },
       link: (scope, element) => {
         const params = scope.params;
         scope.rmin = params.rMin;
 
         const color = d3.scale.category20();
         scope.groupColor = {};
-        for (const key in params.groups) {
-          scope.groupColor[key] = color(key);
-        }
+        scope.$watchCollection('params.groups', () => {
+          for (const key in params.groups) {
+            scope.groupColor[key] = color(key);
+          }
+        });
 
         scope.updateRMin = () => {
           params.rMin = scope.rMin;
@@ -30,7 +94,8 @@ angular.module('riken')
       },
       restrict: 'E',
       scope: {
-        params: '='
+        params: '=',
+        onLoadFile: '='
       },
       templateUrl: 'partials/control.html'
     };
