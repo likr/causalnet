6d86d668d0c33df551fa535c57c8828f38bba41e
Yosuke ONOUE <onoue@likr-lab.com>
2016-06-24 12:21:11 +0900

    update data script

--- data/data.py
+++ data/data.py
@@ -6,24 +6,38 @@ import numpy
 VERSION = 3
 
 layers = [
-    '1 cell',
-    '1-2 cell',
-    '2 cell',
-    '2-4 cell',
-    '4 cell',
-    '4-8 cell',
-    '8 cell',
+    '1 cell | first',
+    '1-2 cell | first',
+    '1-2 cell | last',
+    '2 cell | first',
+    '2 cell | middle',
+    '2 cell | last',
+    '2-4 cell | firstAB',
+    '2-4 cell | firstP1',
+    '2-4 cell | last',
+    '4 cell | first',
+    '4 cell | middle',
+    '4 cell | last',
+    '4-8 cell | firstAB_ap',
+    '4-8 cell | firstEMS',
+    '4-8 cell | firstP2',
+    '4-8 cell | last',
+    '8 cell | first',
 ]
 
 def main():
     variables = list(csv.reader(open('variables.csv')))[1:]
+    timepoint = {row[0]: '{} | {}'.format(row[1], row[2]) for row in csv.reader(open('timepoint.csv'))}
     data = numpy.array([[float(val) if val else 0.0 for val in row[1:]] for row in csv.reader(open('data.csv'))])
     coef = numpy.corrcoef(data.T)
     cells = set()
     variableTypes = set()
     vertices = []
     edges = []
+    verticesIds = {}
     for i, variable in enumerate(variables):
+        if variable[1] not in timepoint:
+            continue
         v_cells = variable[6].split()
         for cell in v_cells:
             cells.add(cell)
@@ -33,8 +47,8 @@ def main():
                 'index': variable[0],
                 'name': variable[1],
                 'description': variable[2],
-                'layer': variable[3],
-                'layerOrder': layers.index(variable[3]),
+                'layer': timepoint[variable[1]],
+                'layerOrder': layers.index(timepoint[variable[1]]),
                 'unit': variable[4],
                 'variableType': variable[5],
                 'cells': v_cells,
@@ -42,13 +56,29 @@ def main():
             },
             'u': variable[0],
         })
-        for j, variable2 in enumerate(variables):
+        verticesIds[variable[1]] = variable[0]
+        # for j, variable2 in enumerate(variables):
+        #     edges.append({
+        #         'd': {
+        #             'r': coef[i, j]
+        #         },
+        #         'u': variable[0],
+        #         'v': variable2[0],
+        #     })
+    paths = [list(row) for row in csv.reader(open('lasso.csv'))]
+    results = paths[0][1:]
+    for row in paths[2:]:
+        reason = '_'.join(row[0].split('_')[1:])
+        for result, value in zip(results, row[1:]):
+            if not value:
+                continue
+            result = '_'.join(result.split('_')[1:])
             edges.append({
                 'd': {
-                    'r': coef[i, j]
+                    'r': float(value),
                 },
-                'u': variable[0],
-                'v': variable2[0],
+                'u': verticesIds[reason],
+                'v': verticesIds[result],
             })
 
     obj = {
